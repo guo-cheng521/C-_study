ca8bbbbfd8a78272317f983e4875a4257901ed04
guo-cheng521 <78196654+guo-cheng521@users.noreply.github.com>
2022-02-04 22:31:24 +0800

    cha_9

--- cha_9/test.cpp
+++ cha_9/test.cpp
@@ -86,52 +86,89 @@
 
 
 // static.cpp -- using a static local variable
+//#include <iostream>
+//// constants
+//const int ArSize = 10;
+//
+//// function prototype
+//void strcount(const char* str);
+//
+//int main()
+//{
+//    using namespace std;
+//    char input[ArSize];
+//    char next;
+//
+//    cout << "Enter a line:\n";
+//    cin.get(input, ArSize);
+//    while (cin)
+//    {
+//        cin.get(next);
+//        while (next != '\n')    // string didn't fit!
+//            cin.get(next);      // dispose of remainder
+//        strcount(input);
+//        cout << "Enter next line (empty line to quit):\n";
+//        cin.get(input, ArSize);
+//    }
+//    cout << "Bye\n";
+//    // code to keep window open for MSVC++
+//    /*
+//    cin.clear();
+//        while (cin.get() != '\n')
+//            continue;
+//        cin.get();
+//    */
+//    return 0;
+//}
+//
+//void strcount(const char* str)
+//{
+//    using namespace std;
+//    static int total = 0;        // static local variable
+//    int count = 0;               // automatic local variable
+//
+//    cout << "\"" << str << "\" contains ";
+//    while (*str++)               // go to end of string
+//        count++;
+//    total += count;
+//    cout << count << " characters\n";
+//    cout << total << " characters total\n";
+//}
+
+
 #include <iostream>
-// constants
-const int ArSize = 10;
-
-// function prototype
-void strcount(const char* str);
-
+using namespace std;
+void other();
+namespace n1
+{
+	int x = 1;
+}
+namespace n2
+{
+	int x = 2;
+}
 int main()
 {
-    using namespace std;
-    char input[ArSize];
-    char next;
+	using namespace n1;
+	cout << x << endl;
+	{
+		int x = 4;
+		cout << x << "," << n1::x << "," << n2::x << endl;
 
-    cout << "Enter a line:\n";
-    cin.get(input, ArSize);
-    while (cin)
-    {
-        cin.get(next);
-        while (next != '\n')    // string didn't fit!
-            cin.get(next);      // dispose of remainder
-        strcount(input);
-        cout << "Enter next line (empty line to quit):\n";
-        cin.get(input, ArSize);
-    }
-    cout << "Bye\n";
-    // code to keep window open for MSVC++
-    /*
-    cin.clear();
-        while (cin.get() != '\n')
-            continue;
-        cin.get();
-    */
-    return 0;
+	}
+	using n2::x;
+	cout << x << endl;
+	other();
+	return 0;
 }
-
-void strcount(const char* str)
+void other()
 {
-    using namespace std;
-    static int total = 0;        // static local variable
-    int count = 0;               // automatic local variable
-
-    cout << "\"" << str << "\" contains ";
-    while (*str++)               // go to end of string
-        count++;
-    total += count;
-    cout << count << " characters\n";
-    cout << total << " characters total\n";
+	using namespace n2;
+	cout << x << endl;
+	{
+		int x = 4;
+		cout << x << "," << n1::x << "," << n2::x << endl;
+	}
+	using n2::x;
+	cout << x << endl;
 }
-
